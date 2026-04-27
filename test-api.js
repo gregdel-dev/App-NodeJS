fb4f6400e116e7507bd3a07b6f099c7356cfdb2c
Grégoire de L <150388661+gregdel-dev@users.noreply.github.com>
2026-04-27 23:22:52 +0200

    update gitignore

--- test-api.js
+++ test-api.js
@@ -2,18 +2,27 @@
 async function testPost() {
   const data = {
     id_emplacement: 2,
-    code_barre : "3024360123459",
+    code_barre : "3024360123459e",
     date_peremption : "2026-123",
-    quantite : 15
+    quantite : 15,
+    nom : "un nom",
+    marque : "marque",
+    poids : 12,
+    url_image : "test",
   };
 
   try {
-    const response = await fetch('http://localhost:3000/api/data_gestion/get/recherche?chaine=ca', {
+    const response = await fetch('http://localhost:3000/api/data_gestion/get/produit', {
       method: 'GET',
-      headers: { 'Content-Type': 'application/json', "x-api-key":"Y+gI#lSP8N&faclf+O?h6geGu@-3truxi0uvunUsTihajud" },
+      headers: { 'Content-Type': 'application/json', "x-api-key":"Y+gI#lSP8N&faclf+O?h6geGu@-3truxi0uvunUsTihajud", "User-Agent": "Mozilla/5.0 (Linux; Android $version; $manufacturer $model) GregApp"},
       //body: JSON.stringify(data)
     });
-
+    const response_Post =await fetch('http://localhost:3000/api/data_gestion/ajout/emplacement', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', "x-api-key":"Y+gI#lSP8N&faclf+O?h6geGu@-3truxi0uvunUsTihajud", "User-Agent": "Mozilla/5.0 (Linux; Android $version; $manufacturer $model) GregApp"},
+      body: JSON.stringify(data)
+    });
+    //const result = await response_Post
     const result = await response.json();
     
     if (response.ok) {
@@ -27,3 +36,4 @@ async function testPost() {
 }
 
 testPost();
+
